18376f6b6983e2397ccc4f4fee5c3470df73e5e9
sh7t <karimgaldonsky@gmail.com>
2025-11-19 19:39:38 +0200

    Init commit

---
+++ EM_HW14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EM_HW14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
