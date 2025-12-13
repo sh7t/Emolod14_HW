dcf4b3f6d797954234b73f5fa9aceda8e5cd0c53
sh7t <karimgaldonsky@gmail.com>
2025-12-13 09:40:19 +0200

    upd: fourth homework uploaded(2/2)

--- EM_HW14/Program.cs
+++ EM_HW14/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace EM_HW14
@@ -21,6 +20,11 @@ namespace EM_HW14
         public static int IntegerInput(string message)
         {
             return Convert.ToInt32(StringInput(message));
+        }
+        public static int IntegerLineInput(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
         }
         public static void ShowError(string message)
         {
@@ -46,7 +50,7 @@ namespace EM_HW14
             return random.Next(max);
         }
     }
-
+    /*
     public class Book
     {
         // fields
@@ -194,8 +198,125 @@ namespace EM_HW14
             library.RemoveAt(index);
         }
     }
+    */
+    /* public class Product
+    {
+        // fields
+        private static int autoInc = 0;
+        private readonly int id;
+        private string name = "";
+        private double price = 0;
+        private uint quantity = 0; // experimental uint
+
+        // init 
+        public Product(string name, double price, uint quantity)
+        {
+            id = autoInc++;
+
+            setName(name);
+            setPrice(price);
+            setQuantity(quantity);
+        }
+
+        // getset'ters
+        public int getId() => id;
+
+        public string getName() => name;
+        public void setName(string name)
+        {
+            if (name.Trim().Length < 2)
+                throw new Exception("Expected longer product name. Entered information is too short.");
+            this.name = name.Trim();
+        }
+        public double getPrice() => price;
+        public void setPrice(double price)
+        {
+            if (price < 0)
+                throw new Exception("Expected real price. Entered information is impossible to be price.");
+            this.price = Math.Round(price, 2);        
+        }
+
+        public uint getQuantity() => quantity;
+        public void setQuantity(uint quantity) { this.quantity = quantity; }
+    }
+    public static class ProductsService
+    {
+        // fields
+        private static List<Product> products = new List<Product>();
+
+        // funcs
+        private static void CheckProductsEmptiness()
+        {
+            if (products.Count == 0)
+                throw new Exception("Products list is empty at the moment. Come back later!");
+        }
+        private static int GetIndexById(int id)
+        {
+            CheckProductsEmptiness();
+            CheckIsIdInProducts(id);
+
+            return products.FindIndex(product => product.getId() == id);
+        }
+        public static void CheckIsIdInProducts(int id)
+        {
+            if (products.Find(p => p.getId() == id) == null)
+                throw new Exception("Entered ID is out of products list's range!");
+        }
+        public static void AddProduct(Product product)
+        {
+            if ((products.Find(prdct => prdct.getName().ToLower() == product.getName().ToLower())) != null)
+                throw new Exception($"{product.getName()} is already in products list!");
+            products.Add(product);
+            Console.WriteLine($"You successfully added {product.getName()} for {product.getPrice()}!");
+        }
+        public static void ShowAllProducts()
+        {
+            CheckProductsEmptiness();
+
+            Console.WriteLine("ID Product");
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"{product.getId()} | {product.getName()} for {product.getPrice()} - {product.getQuantity()} pcs.");
+            }
+        }
+        public static void UpdateProduct(int id, double price) { products[GetIndexById(id)].setPrice(price); }
+        public static void UpdateProduct(int id, uint quantity) { products[GetIndexById(id)].setQuantity(quantity); }
+        public static void UpdateProduct(int id, double price, uint quantity)
+        {
+            UpdateProduct(id, price);
+            UpdateProduct(id, quantity);
+        }
+
+        public static void RemoveProduct(int id) { products.RemoveAt(GetIndexById(id)); }
+        public static void FindProduct(string str)
+        {
+            CheckProductsEmptiness();
+            List<Product> found = products.FindAll(product => product.getName().ToLower().Contains(str.ToLower()));
+            if (found.Count == 0)
+                throw new Exception($"Nothing found in products list by request {str}!");
+            Console.WriteLine("ID   Found product");
+            foreach (Product p in found)
+            {
+                Console.WriteLine($"{p.getId()} | {p.getName()} for {p.getPrice()} - {p.getQuantity()} pcs.");
+            }
+        }
+        public static void BuyProduct(int id, double budget)
+        {
+            int index = GetIndexById(id);
+
+            if (products[index].getQuantity() < Convert.ToUInt32(budget / products[index].getPrice()))
+                throw new Exception("Not enough products to sell!");
+
+            Console.WriteLine($"You successfully bought {products[index].getName()} for {products[index].getPrice()}/pcs. " +
+                $"in the amount of {Convert.ToUInt32(budget / products[index].getPrice())}");
+            Console.WriteLine($"Your rest: {budget - Convert.ToUInt32(budget / products[index].getPrice()) * products[index].getPrice()}");
+            UpdateProduct(id, (products[index].getQuantity() - Convert.ToUInt32(budget / products[index].getPrice())));
+
+        }
 
 
+    }
+    */ 
     internal class Program
     {
         static void Main(string[] args)
@@ -203,10 +324,45 @@ namespace EM_HW14
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.Unicode;
 
+            // куча ексепшенів обумовлена тим, що інтерфейси програм повинні бути зацикленим трай-кечами
+            // (але я облінився їх робить)
+
             // 1.
             // from 54 to 200
 
             // 2.
+            /*
+            try
+            {
+                ProductsService.AddProduct(new Product("Laptop", 12999.00, 34));
+                ProductsService.AddProduct(new Product("Smartphone", 8967.00, 78));
+                ProductsService.AddProduct(new Product("Powerbank", 1270.00, 12));
+                ConsoleHelper.ShowSeparator();
+
+                ProductsService.ShowAllProducts();
+                ConsoleHelper.ShowSeparator();
+
+                int choosenId = ConsoleHelper.IntegerLineInput("Which product are you interested in? ID: ");
+                ProductsService.CheckIsIdInProducts(choosenId);
+
+                double budget = Convert.ToDouble(ConsoleHelper.IntegerLineInput("Your budget: "));
+                if (budget <= 0)
+                    throw new Exception("Expected real budget. Entered budget doesn't seem to be real!");
+
+                ProductsService.BuyProduct(choosenId, budget);
+                ConsoleHelper.ShowSeparator();
+                ProductsService.ShowAllProducts();
+
+            }
+            catch (FormatException)
+            {
+                ConsoleHelper.ShowError("Incorrect input format!");
+            }
+            catch (Exception ex)
+            {
+                ConsoleHelper.ShowError(ex.Message);
+            }
+            */
         }
     }
 }
